9b4accb180e503c2f3661d139bffb7cc9d9f84a8
Daniel Hazelbaker <daniel@blueboxmoon.com>
2018-10-15 15:24:30 -0700

    Various updates to have proper TV app submission.

--- Sample.Android/MainActivity.cs
+++ Sample.Android/MainActivity.cs
@@ -1,10 +1,12 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Support.V7.App;
 
 namespace Sample.Android
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity( Label = "@string/app_name", Theme = "@style/AppTheme" )]
+    [IntentFilter( new[] { Intent.ActionMain }, Categories = new[] { Intent.CategoryLeanbackLauncher } )]
     public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
