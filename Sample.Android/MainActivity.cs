278aa0f4b92851772f4373214c03d9a04f887c62
Daniel Hazelbaker <daniel@blueboxmoon.com>
2018-10-16 09:48:03 -0700

    Need MainLauncher = true to submit app apparently

--- Sample.Android/MainActivity.cs
+++ Sample.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.Support.V7.App;
 
 namespace Sample.Android
 {
-    [Activity( Label = "@string/app_name", Theme = "@style/AppTheme" )]
+    [Activity( Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true )]
     [IntentFilter( new[] { Intent.ActionMain }, Categories = new[] { Intent.CategoryLeanbackLauncher } )]
     public class MainActivity : AppCompatActivity
     {
